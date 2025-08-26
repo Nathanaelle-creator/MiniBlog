057f03b9235d0b19d95bdb43e6f1f97594308d87
Nathanaelle-creator <nathanaelle.latouche@epitech.eu>
2025-08-26 20:47:13 +0200

    ADD: ajout des actions create et update

--- app/controllers/blagues_controller.rb
+++ app/controllers/blagues_controller.rb
@@ -1,8 +1,37 @@
 class BlaguesController < ApplicationController
-   def index
-       @blague = Blague.all
-   end
-  def show
-   @blague = Blague.find(params[:id])
+  def index
+      @blague = Blague.all
   end
+
+  def show
+    @blague = Blague.find(params[:id])
+  end
+
+  def new
+    @blague= Blague.new
+  end 
+
+ def create
+  @blague = Blague.new(params_blague)
+
+  if @blague.save
+    redirect_to @blague
+  else
+    render :new
+  end
+
+  def edit
+  end
+
+  def upade
+  end
+end
+
+
+
+  private 
+  def params_blague
+    params.require(:blague).permit(:nom,:contenu,:auteur)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-root 'home#index'
-get 'home',to:'home#index'
-resources :blagues, only:[:show, :index]
+  root 'home#index'
+  get 'home', to: 'home#index'
+  
+  resources :blagues, only: [:index, :show, :new, :create,:update,:edit]
 end
