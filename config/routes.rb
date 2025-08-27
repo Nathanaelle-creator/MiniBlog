b72b753cdcb2bf8cd1e09d5f88db9242fecd6ef2
Nathanaelle-creator <nathanaelle.latouche@epitech.eu>
2025-08-27 11:23:24 +0200

    ADD: ajout du style dans toutes les pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home', to: 'home#index'
   
-  resources :blagues, only: [:index, :show, :new, :create,:update,:edit]
+  resources :blagues
   resources :lessons , only: [:index, :show]
 end
